9d55627a73fc56a839018586f31dc8f30fb5707b
emTrieuWeb <buinhattrieu0509@gmail.com>
2024-01-18 12:34:38 +0700

    api

--- functions/api.js
+++ functions/api.js
@@ -8,11 +8,51 @@ const router = require("./routes/index");
 app.use(express.json());
 mongoose
   .connect(
-    `mongodb+srv://root:123@cluster0.1wrjmpx.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://root:123@cluster0.ox2upqw.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => console.log("Database Connected!"));
 
 app.use(cors());
+//Get all students
+// router.get("/", (req, res) => {
+//   res.send("App is running..");
+// });
+
+// //Create new record
+// router.post("/add", (req, res) => {
+//   res.send("New record added.");
+// });
+
+// //delete existing record
+// router.delete("/", (req, res) => {
+//   res.send("Deleted existing record");
+// });
+
+// //updating existing record
+// router.put("/", (req, res) => {
+//   res.send("Updating existing record");
+// });
+
+// //showing demo records
+// router.get("/demo", (req, res) => {
+//   res.json([
+//     {
+//       id: "001",
+//       name: "Smith",
+//       email: "smith@gmail.com",
+//     },
+//     {
+//       id: "002",
+//       name: "Sam",
+//       email: "sam@gmail.com",
+//     },
+//     {
+//       id: "003",
+//       name: "lily",
+//       email: "lily@gmail.com",
+//     },
+//   ]);
+// });
 
 app.use("/.netlify/functions/api", router);
 module.exports.handler = serverless(app);
